7c357a5130ea13d8bf49f670f941023a021448bd
Herbie <hjrand1005@gmail.com>
2022-07-30 09:12:45 -0400

    ADD: src

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/rajshekhar26/cleanfolio'
+      className='link footer__link'
+    >
+      Credit to Raj Shekhar's Cleanfolio
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,69 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://hrand1005.github.io',
+  title: 'HR.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Herbie Rand',
+  role: 'Software Engineer, Violinist, Weightlifter',
+  description: 'Currently pursuing a Master\'s Degree in Software Engineering',
+    // 'Currently pursuing a Masters Degree in Software Engineering',
+  resume: 'https://github.com/hrand1005/general-portfolio/resume.pdf',
+  social: {
+    linkedin: 'https://linkedin.com/in/herbie-rand-a863851a0/',
+    github: 'https://github.com/hrand1005',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'mcsweeney',
+    description:
+      'Automatic youtube content generator',
+    stack: ['Go', 'Bash' ],
+    sourceCode: 'https://github.com/hrand1005/mcsweeney',
+    // livePreview: 'https://github.com/',
+  },
+  {
+    name: 'Training Notebook',
+    description:
+      'API for a training (weightlifting) notebook written in Go',
+    stack: ['Gin', 'Go', 'Bash'],
+    sourceCode: 'https://github.com/hrand1005/training-notebook',
+    // livePreview: 'https://github.com',
+  },
+  {
+    name: 'grafapyAPI',
+    description:
+      'Python Library for creating Grafana Dashboards',
+    stack: ['Python'],
+    sourceCode: 'https://github.com/hrand1005/grafapyAPI',
+    // livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'Go',
+  'Gin',
+  'Gorilla/Mux',
+  'Python',
+  'Flask',
+  'Behave',
+  'Bash',
+  'Jenkins',
+  'Groovy',
+  'Docker'
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'hjrand1005@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
